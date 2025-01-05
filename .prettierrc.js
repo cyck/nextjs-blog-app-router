18832faf92c45147a2581daa642c1762a8640702
Bruno Fernandes <bruno.r.fernandes31@gmail.com>
2025-01-05 16:10:42 +0000

    feat: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+const config = {
+  arrowParens: 'always', // Include parentheses around a sole arrow function parameter
+  bracketSameLine: false, // Put the > of a multi-line HTML or JSX element alone on the next line
+  bracketSpacing: true, // Print spaces between brackets in object literals
+  endOfLine: 'lf', // Line endings in files
+  jsxSingleQuote: true, // Use single quotes instead of double quotes in JSX
+  printWidth: 120, // The line length that the printer will wrap on
+  quoteProps: 'consistent', // Using quotes in object properties should be consistent
+  semi: true, // Print semicolons at the ends of statements
+  singleAttributePerLine: true, // Enforce single attribute per line in HTML and JSX
+  singleQuote: true, // Use single quotes instead of double quotes
+  tabWidth: 2, // The number of spaces per indentation-level
+  trailingComma: 'all', // Print trailing commas wherever possible in multi-line comma-separated syntactic structures
+  useTabs: false, // Indent lines with spaces instead of tabs
+};
+
+export default config;
